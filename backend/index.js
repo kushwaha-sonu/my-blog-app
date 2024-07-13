62c2518ce679388d765f47208938b38e85048a09
Sonu kumar <104054249+kushwaha-sonu@users.noreply.github.com>
2024-07-13 13:03:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 
-app.use(
-  cors({
-    origin: "https://my-blog-app-xscl-hyohaj3bw-sonu-kumars-projects-beca3941.vercel.app",
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type']
-  })
-);
+app.use(cors({
+       origin: 'https://my-blog-app-xscl.vercel.app', // Allow requests only from this origin
+       methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+       allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+       credentials: true // Allow sending cookies and authentication headers
+   }));
 
 
 const connectDB = require("./src/helper/dB");
